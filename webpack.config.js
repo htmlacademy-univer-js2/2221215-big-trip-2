f6cdd0f2ca4e89fac98873571b06567b1dc907e6
Иван Книженцев <prog63@iMac-User.local>
2023-03-21 10:50:43 +0500

    change dependes

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,5 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require("path");
-const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
-const isProduction = process.env.NODE_ENV == "production";
-
 
 module.exports = {
   entry: './src/main.js',
