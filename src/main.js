c746be8bb09c8ab823c911dca4123a1d668f775d
Иван Книженцев <prog63@iMac-User.local>
2023-03-28 10:59:21 +0500

    2

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import FilterView from './view/filter.js';
 import TripEventsPresenter from './presenter/trip-events-presenter.js';
 import MenuView from './view/menu.js';
+import PointsModel from './model/point-model.js';
 import { render } from './render.js';
 
 const siteHeaderElement = document.querySelector('.trip-main');
 const siteMainElement = document.querySelector('.page-main');
-const tripPresenter = new TripEventsPresenter();
+const tripPresenter = new TripEventsPresenter(siteMainElement.querySelector('.trip-events'));
+const pointModel = new PointsModel();
 
 render(new FilterView(), siteHeaderElement.querySelector('.trip-controls__filters'));
 render(new MenuView(), siteHeaderElement.querySelector('.trip-controls__navigation'));
 
-tripPresenter.init(siteMainElement.querySelector('.trip-events'));
+tripPresenter.init(pointModel);
