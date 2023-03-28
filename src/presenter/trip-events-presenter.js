bd9b45acc40b124b25d024126c375200fb87d60f
Иван Книженцев <prog63@iMac-User.local>
2023-03-28 10:59:21 +0500

    2

--- src/presenter/trip-events-presenter.js
+++ src/presenter/trip-events-presenter.js
@@ -6,20 +6,19 @@ import SortView from '../view/sort.js';
 import { render } from '../render.js';
 
 export default class TripEventsPresenter {
-  constructor() {
+  constructor(tripContainer) {
     this.eventsList = new EventsView();
+    this.tripContainer = tripContainer;
   }
 
-  init(tripContainer) {
-    this.tripContainer = tripContainer;
+  init(pointsModel) {
+    this.pointsModel = pointsModel;
+    this.routePoints = [...this.pointsModel.getPoints()];
 
     render(new SortView(), this.tripContainer);
     render(this.eventsList, this.tripContainer);
-    render(new FormEditView(), this.eventsList.getElement());
+    render(new FormEditView(this.routePoints[0]), this.eventsList.getElement());
 
-    Array.from({ length: 3 }, () => new RoutePointView())
-      .forEach((point) => render(point, this.eventsList.getElement()));
-
-    render(new FormCreateView(), this.eventsList.getElement());
+    this.routePoints.forEach((routePoint) => render(new RoutePointView(routePoint), this.eventsList.getElement()));
   }
 }
